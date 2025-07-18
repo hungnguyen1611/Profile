01e95698f36ce8e60c52a397ba117e72f7d1abc9
hungnguyen16112003 <nguyenquochung16112003@gmail.com>
2025-07-18 19:40:03 +0700

    update api_url_production

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -4,7 +4,7 @@ if (import.meta.env.MODE === "development") {
   API_ROOT = "http://localhost:3000";
 }
 if (import.meta.env.MODE === "production") {
-  API_ROOT = "https://hungnguyen1611.online";
+  API_ROOT = "https://hungnguyen1611.online/api/profile";
 }
 
 export default API_ROOT;
